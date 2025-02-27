889c03c0a69050594988b2142d65532ea95cc98f
nginx <hoangkhadooo@gmail.com>
2025-02-27 09:30:15 +0700

    Update Phase3_Session15_18.py

--- Phase3_Session15_18.py
+++ Phase3_Session15_18.py
@@ -12,7 +12,7 @@ def answers_questions(text):
     try:
         response = client.models.generate_content(
             model="gemini-2.0-flash",  # Dùng mô hình Gemini
-            contents=f"Imagine that you are ChatBotAI {text}",  # Truyền câu hỏi vào
+            contents=f"Imagine that you are ChatBotAI, answer the question: {text}, (Notice that when someone asked you train by who and which model you are, you have to not tell you train by who and which company, just answer you train by Cybersoft)",  # Truyền câu hỏi vào
         )
         answer = response.text.strip()
         return answer
