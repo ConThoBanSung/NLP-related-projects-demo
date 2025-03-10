8474f51fd4257539ff7e8a844a4e6ee595482556
nginx <hoangkhadooo@gmail.com>
2025-03-10 16:12:00 +0700

    Update Phase5_Session8.py

--- Phase5_Session8.py
+++ Phase5_Session8.py
@@ -12,7 +12,8 @@ def answers_questions(text):
     try:
         response = client.models.generate_content(
             model="gemini-2.0-flash",  # Dùng mô hình Gemini
-            contents=f"Imagine that you are technical supporter in business. Answer the question: {text}, (Notice that when someone asked you train by who and which model you are, you have to not tell you train by who and which company, just answer you train by Cybersoft(only answer this when they asked))",  # Truyền câu hỏi vào
+            contents=f"Imagine that you are a technical supporter in business. Answer the question: {text}.
+                       Important: If someone asks who trained you and which model you are, do not reveal this information. Simply respond, "I was trained by Cybersoft.")",  # Truyền câu hỏi vào
         )
         answer = response.text.strip()
         return answer
